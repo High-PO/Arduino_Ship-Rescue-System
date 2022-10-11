8436d50f50f890693dd597056b18a23b34590ba3
오승진 <oseungjin@oseungjin-ui-MacBookPro.local>
2022-10-12 00:56:20 +0900

    Back UP Arduino Code

--- src/main.ino
+++ src/main.ino
@@ -1,16 +1,10 @@
 #include <SPI.h>
 #include <MFRC522.h>
- 
-const int RST_PIN = 9;
-const int SS_PIN = 10;
- 
 MFRC522 mfrc522(SS_PIN, RST_PIN);   // MFRC522 인스턴스
- 
-byte CardUidByte[4] = {0x76, 0xA8, 0x9F, 0xBB}; //card UID
-boolean state = false; //Servo Motor 상태값
- 
+byte CardUidByte[4] = {0x76, 0xA8, 0x9F, 0xBB}; // Blue Keyring Card UID
+const int RST_PIN = 9; // 
+const int SS_PIN = 10;
 const int TONEPIN = 6;
- 
 void setup() {
   Serial.begin(9600);  
   while (!Serial);     
@@ -19,13 +13,12 @@ void setup() {
   Serial.println(F("Warning: this example clears your mifare UID, use with care!"));
   delay(50);
 }
- 
 void loop() {
-  int level = analogRead(A0);  // 수분센서의 신호를 측정합니다.
-  if (level > 580){
-    Serial.println("Warring");   //시리얼 모니터에 값을 출력합니다.
-    tone(TONEPIN ,523,1000/8);
-    delay(1000/4*1.30);
+  int level = analogRead(A0);  // A0포트에 연결되어있느 수위센서의 신호를 측정
+  if (level > 580){ //만약 수위센서에 580이상의 값이 입력된다.
+    Serial.println("Warring");   // 시리얼 모니터로 "Warring"이라는 문구를 출력한다.
+    tone(TONEPIN ,523,1000/8); // 비프음이 울리게한다.
+    delay(1000/4*1.30); // 딜레이를 준다.
   }  
   // 새 카드 확인
   if ( ! mfrc522.PICC_IsNewCardPresent()) return; 
@@ -37,11 +30,11 @@ void loop() {
     state=!state;
     if(state == true){
       digitalWrite(IN1, HIGH); //정방향회전
-      digitalWrite(IN2, LOW);      
+      digitalWrite(IN2, LOW); 
       Serial.println("Open");
-      //카드 확인 메세지 음           
-      noTone(TONEPIN );            
-      delay(1000);
+      // //카드 확인 메세지 음           
+      // noTone(TONEPIN);            
+      // delay(1000);
     }
   }
 }
