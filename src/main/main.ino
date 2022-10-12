da6fd29a22443a2e7f72fb13de4b1b35a064eb2c
오승진 <oseungjin@oseungjin-ui-MacBookPro.local>
2022-10-12 11:53:04 +0900

    end

--- src/main/main.ino
+++ src/main/main.ino
@@ -1,10 +1,13 @@
 #include <SPI.h>
 #include <MFRC522.h>
-MFRC522 mfrc522(SS_PIN, RST_PIN);   // MFRC522 인스턴스
 byte CardUidByte[4] = {0x76, 0xA8, 0x9F, 0xBB}; // Blue Keyring Card UID
 const int RST_PIN = 9; // 
 const int SS_PIN = 10;
 const int TONEPIN = 6;
+int IN1 = 2; //IN1을 2번핀으로
+int IN2 = 3; //IN2을 3번핀으로
+MFRC522 mfrc522(SS_PIN, RST_PIN);
+boolean state = false; //Servo Motor 상태값
 void setup() {
   Serial.begin(9600);  
   while (!Serial);     
@@ -29,9 +32,10 @@ void loop() {
         mfrc522.uid.uidByte[2] == CardUidByte[2] && mfrc522.uid.uidByte[3] == CardUidByte[3] ){    
     state=!state;
     if(state == true){
-      digitalWrite(IN1, HIGH); //정방향회전
+      digitalWrite(IN1, HIGH);
       digitalWrite(IN2, LOW); 
       Serial.println("Open");
+      tone(TONEPIN ,523,1000/8); // 비프음이 울리게한다.
       // //카드 확인 메세지 음           
       // noTone(TONEPIN);            
       // delay(1000);
