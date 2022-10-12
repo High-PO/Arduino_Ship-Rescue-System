58f74f0dc72e5cc327ea97b7423e80cc7f74054f
오승진 <oseungjin@oseungjin-ui-MacBookPro.local>
2022-10-12 12:47:25 +0900

    bob

--- src/main/main.ino
+++ src/main/main.ino
@@ -1,7 +1,7 @@
 #include <SPI.h>
 #include <MFRC522.h>
 byte CardUidByte[4] = {0x76, 0xA8, 0x9F, 0xBB}; // Blue Keyring Card UID
-const int RST_PIN = 9; // 
+const int RST_PIN = 9; //
 const int SS_PIN = 10;
 const int TONEPIN = 6;
 int IN1 = 2; //IN1을 2번핀으로
@@ -9,8 +9,8 @@ int IN2 = 3; //IN2을 3번핀으로
 MFRC522 mfrc522(SS_PIN, RST_PIN);
 boolean state = false; //Servo Motor 상태값
 void setup() {
-  Serial.begin(9600);  
-  while (!Serial);     
+  Serial.begin(9600);
+  while (!Serial);
   SPI.begin();         //SPI 시작
   mfrc522.PCD_Init();  //MFRC522 card 초기화
   Serial.println(F("Warning: this example clears your mifare UID, use with care!"));
@@ -22,22 +22,22 @@ void loop() {
     Serial.println("Warring");   // 시리얼 모니터로 "Warring"이라는 문구를 출력한다.
     tone(TONEPIN ,523,1000/8); // 비프음이 울리게한다.
     delay(1000/4*1.30); // 딜레이를 준다.
-  }  
+  }
   // 새 카드 확인
-  if ( ! mfrc522.PICC_IsNewCardPresent()) return; 
+  if ( ! mfrc522.PICC_IsNewCardPresent()) return;
   // 카드 읽기
   if ( ! mfrc522.PICC_ReadCardSerial()) return;
   //읽은 Card UID와 등록된 Card UID가 일치하는 확인
   if (mfrc522.uid.uidByte[0] == CardUidByte[0] && mfrc522.uid.uidByte[1] == CardUidByte[1] &&
-        mfrc522.uid.uidByte[2] == CardUidByte[2] && mfrc522.uid.uidByte[3] == CardUidByte[3] ){    
+        mfrc522.uid.uidByte[2] == CardUidByte[2] && mfrc522.uid.uidByte[3] == CardUidByte[3] ){
     state=!state;
     if(state == true){
       digitalWrite(IN1, HIGH);
-      digitalWrite(IN2, LOW); 
+      digitalWrite(IN2, LOW);
       Serial.println("Open");
       tone(TONEPIN ,523,1000/8); // 비프음이 울리게한다.
-      // //카드 확인 메세지 음           
-      // noTone(TONEPIN);            
+      // //카드 확인 메세지 음
+      // noTone(TONEPIN);
       // delay(1000);
     }
   }
